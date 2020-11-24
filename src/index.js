8599afdbcde3c19dc6e7e006d9fea7c0016ebe0b
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-11-24 00:30:36 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -3,59 +3,38 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 //stateless functional component
-const firstBook = {
+const books = [{
   img : 'https://images-na.ssl-images-amazon.com/images/I/A1Ee%2BYjn92L._AC_UL200_SR200,200_.jpg',
   title : 'The Polar Express',
   author : 'Chris Van Allsburg',
-}
-
-const secBook = {
+},
+{
   img : 'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg',
   title : 'Greenlights',
   author : 'Matthew McConaughey',
-}
+}];
 
-const thirdBook = {
-  img : 'https://images-na.ssl-images-amazon.com/images/I/91F6EG8aJ%2BL._AC_UL200_SR200,200_.jpg',
-  title : 'The Monster at the End of this Book',
-  author : 'Grover from Sesame Street'
-}
-
+const names = ['john', 'peter','susan'];
+const newName = names.map((name) => {
+  return <h1>{name}</h1>
+});
+console.log(newName)
 function BookList() { //This is our component, React knows with capital letter
   return (
     <section className="booklist">
-      <Book 
-      img={firstBook.img} 
-      title={firstBook.title} 
-      author={firstBook.author}
-      />
-      <Book 
-      img={secBook.img} 
-      title={secBook.title} 
-      author={secBook.author}
-      />
-      <Book
-      img = {thirdBook.img}
-      title = {thirdBook.title}
-      author = {thirdBook.author}
-      >
-        <p>
-          This book is about a super hero 
-          who can kill Superman.
-        </p>
-      </Book>
+      {newName}
     </section>
   );
 }
 
 const Book = (props) => {
-  //const {img, title, author} = props;
+  const {img, title, author} = props;
   console.log(props);
   return (
     <article className="book">
-      <img src={props.img} alt=""/>
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt=""/>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
       {props.children}
     </article>
   );
