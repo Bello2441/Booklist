336cafc97d30283266a4d70943c0b71001795fd1
Bello2441 <rosenbergbello.@lc.cuny.edu'j>
2020-11-25 00:34:08 -0500

    added extra book and onClick

--- src/index.js
+++ src/index.js
@@ -3,39 +3,49 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 //stateless functional component
-const books = [{
+const books = [
+  { id: 1,
   img : 'https://images-na.ssl-images-amazon.com/images/I/A1Ee%2BYjn92L._AC_UL200_SR200,200_.jpg',
   title : 'The Polar Express',
   author : 'Chris Van Allsburg',
 },
-{
+{ id: 2,
   img : 'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg',
   title : 'Greenlights',
   author : 'Matthew McConaughey',
+},
+{ id : 3,
+  img : 'https://images-na.ssl-images-amazon.com/images/I/41cW7tUznyL.__AC_SY300_QL70_ML2_.jpg',
+  title : 'Sky Scout',
+  author : 'Yu-Gi-Oh',
 }];
 
-const names = ['john', 'peter','susan'];
-const newName = names.map((name) => {
-  return <h1>{name}</h1>
-});
-console.log(newName)
 function BookList() { //This is our component, React knows with capital letter
   return (
     <section className="booklist">
-      {newName}
+      {books.map((book)=>{
+        return (
+        <Book key={book.id} {...book}></Book>
+          );
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const {img, title, author} = props;
   console.log(props);
+  const {img, title, author} = props;
+  const clickHandler = () => {
+    alert('Hello World');
+  }
   return (
     <article className="book">
       <img src={img} alt=""/>
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
+      <button type='button' onClick={clickHandler}>
+        Hello!
+      </button>
     </article>
   );
 }
